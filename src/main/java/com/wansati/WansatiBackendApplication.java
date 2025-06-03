082e12faf5ec1e2de29e638e89a047895ee16189
Alison Pariela <parielaalison@gmail.com>
2025-06-03 15:11:42 +0400

    initial files

---
+++ src/main/java/com/wansati/WansatiBackendApplication.java
@@ -0,0 +1,11 @@
+package com.wansati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WansatiBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WansatiBackendApplication.class, args);
+    }
+}
